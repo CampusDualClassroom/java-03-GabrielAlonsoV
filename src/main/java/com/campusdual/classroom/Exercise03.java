a4436d25764e7c383d5467e939610ce8bb160ab1
Gabriel Alonso Varela <gabrielalonsovarela1@gmail.com>
2024-09-25 12:52:41 +0200

    Ejercicio 3 Completado

--- src/main/java/com/campusdual/classroom/Exercise03.java
+++ src/main/java/com/campusdual/classroom/Exercise03.java
@@ -17,12 +17,12 @@ public class Exercise03 {
     //Esto es un procedimiento, ya que no devuelve un valor
     // TODO ↓: Tiene que comprobar si el usuario que recibe por parámetro es "Sebas" y su contraseña es "sebas01". Si es correcto, que muestre el mensaje de la función greetings() y si no que muestre el mensaje de la función error()
     public static void checkUser(String user, String pass) {
+        String username = "Sebas";
+
         if (user.equals("Sebas")&&pass.equals("sebas01")){
             System.out.println(greetings(user));
-        }else if (!user.equals("Sebas")&&pass.equals("sebas01")){
-            System.out.println("Ups. No pudimos validar tus datos. Tu nombre de usuario no es Sebas");
         } else{
-            System.out.println(error(user));
+            System.out.println(error(username));
         }
     }
 
